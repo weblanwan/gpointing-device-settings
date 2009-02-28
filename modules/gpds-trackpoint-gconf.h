0bef56d9e202b0fcf5ad246469b974ef7ad2af70
Hiroyuki Ikezoe <hiikezoe@src.gnome.org>
2009-02-28 05:22:06 +0000

    added key name defined file.
    svn path=/trunk/; revision=54

---
+++ modules/gpds-trackpoint-gconf.h
@@ -0,0 +1,37 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ *  Copyright (C) 2009 Hiroyuki Ikezoe  <poincare@ikezoe.net>
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef __GPDS_TRACK_POINT_GCONF_H__
+#define __GPDS_TRACK_POINT_GCONF_H__
+
+#define GPDS_TRACK_POINT_GCONF_DIR                   "/desktop/gnome/peripherals/trackpoint"
+#define GPDS_TRACK_POINT_MIDDLE_BUTTON_EMULATION_KEY GPDS_TRACK_POINT_GCONF_DIR "/middle_button_emulation"
+#define GPDS_TRACK_POINT_MIDDLE_BUTTON_TIMEOUT_KEY   GPDS_TRACK_POINT_GCONF_DIR "/middle_button_timeoue"
+#define GPDS_TRACK_POINT_WHEEL_EMULATION_KEY         GPDS_TRACK_POINT_GCONF_DIR "/wheel_emulation"
+#define GPDS_TRACK_POINT_WHEEL_EMULATION_INERTIA_KEY GPDS_TRACK_POINT_GCONF_DIR "/wheel_emulation_inetia"
+#define GPDS_TRACK_POINT_WHEEL_EMULATION_X_AXIS_KEY  GPDS_TRACK_POINT_GCONF_DIR "/wheel_emulation_x_axis"
+#define GPDS_TRACK_POINT_WHEEL_EMULATION_Y_AXIS_KEY  GPDS_TRACK_POINT_GCONF_DIR "/wheel_emulation_y_axis"
+#define GPDS_TRACK_POINT_WHEEL_EMULATION_TIMEOUT_KEY GPDS_TRACK_POINT_GCONF_DIR "/wheel_emulation_timeout"
+#define GPDS_TRACK_POINT_WHEEL_EMULATION_BUTTON_KEY  GPDS_TRACK_POINT_GCONF_DIR "/wheel_emulation_button"
+#define GPDS_TRACK_POINT_DRAG_LOCK_BUTTONS_KEY       GPDS_TRACK_POINT_GCONF_DIR "/drag_lock_buttons"
+
+#endif /* __GPDS_TRACK_POINT_GCONF_H__ */
+/*
+vi:ts=4:nowrap:ai:expandtab:sw=4
+*/
