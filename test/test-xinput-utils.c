12633816d7e0c39f45258872f3d305074742000b
Hiroyuki Ikezoe <hiikezoe@src.gnome.org>
2009-03-11 03:19:07 +0000

    cleanup.
    svn path=/trunk/; revision=205

--- test/test-xinput-utils.c
+++ test/test-xinput-utils.c
@@ -12,6 +12,8 @@ void test_get_device_num_buttons (void);
 static GError *error;
 static XDevice *device;
 
+#define DEVICE_NAME "Macintosh mouse button emulation"
+
 void
 setup (void)
 {
@@ -44,14 +46,14 @@ void
 test_get_device_info (void)
 {
     XDeviceInfo *device_info = NULL; 
-    device_info = gpds_xinput_utils_get_device_info("Macintosh mouse button emulation");
+    device_info = gpds_xinput_utils_get_device_info(DEVICE_NAME);
     cut_assert(device_info);
 }
 
 void
 test_open_device (void)
 {
-    device = gpds_xinput_utils_open_device("Macintosh mouse button emulation", &error);
+    device = gpds_xinput_utils_open_device(DEVICE_NAME, &error);
     cut_assert(device);
 
     gcut_assert_error(error);
@@ -62,10 +64,10 @@ test_get_device_num_buttons (void)
 {
     gshort n_buttons;
 
-    n_buttons = gpds_xinput_utils_get_device_num_buttons("Macintosh mouse button emulation", &error);
-    cut_assert_equal_int(32, n_buttons);
-
+    n_buttons = gpds_xinput_utils_get_device_num_buttons(DEVICE_NAME, &error);
     gcut_assert_error(error);
+
+    cut_assert_equal_int(32, n_buttons);
 }
 
 /*
